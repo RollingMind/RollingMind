7115090faaccc58b4fea2b67903083c19df9f095
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2024-01-27 21:11:19 +0900

    :sparkles: mysql 연동 완료

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000
+const mysql = require('mysql');
+const connction = mysql.createConnection({
+  host : 'localhost',
+  user : 'test',
+  password : '1111',
+  database : 'test'
+});
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -8,3 +17,12 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(3000, () => {
   console.log('서버가 3000번 포트에서 실행 중입니다.');
 });
+
+connction.connect();
+
+connction.query('Select * from Users', (error, rows, fields) => {
+  if (error) throw error;
+  console.log('User info is: ', rows);
+});
+
+connction.end();

7c4fccfc5576586d6c920e1dbea416ed85f32789
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2024-01-27 22:26:07 +0900

    :sparkles: route 설정

--- test.js
+++ test.js
@@ -1,28 +1,27 @@
 const express = require('express');
-const app = express();
-const port = 3000
+
 const mysql = require('mysql');
-const connction = mysql.createConnection({
-  host : 'localhost',
-  user : 'test',
-  password : '1111',
-  database : 'test'
+const dbconfig = require('../RollingMind/database.js');
+const connction = mysql.createConnection(dbconfig);
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.send('Root');
+})
+
+app.get('/users', (req, res) => {
+  connction.query('Select * from Users', (error, rows, fields) => {
+    if (error) throw error;
+    console.log('User info is: ', rows);
+    res.send(rows);
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-// 서버 실행
-app.listen(3000, () => {
-  console.log('서버가 3000번 포트에서 실행 중입니다.');
-});
-
-connction.connect();
-
-connction.query('Select * from Users', (error, rows, fields) => {
-  if (error) throw error;
-  console.log('User info is: ', rows);
-});
-
-connction.end();

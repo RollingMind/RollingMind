69cf996972a82044097d1a619f36434b4b66b11f
jiyeon11 <leejiyeon415@gmail.com>
2024-02-10 16:04:02 +0900

    ✨ 좋아요 누른 템플릿 가져오기

--- create-room.js
+++ create-room.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -6,27 +7,29 @@ const db = require('./lib/crdb');
 app.use(express.json());
 
 app.get('/create-room', async (request, response) => {
-    const inviteCode = await generateRoomId(); //초대 코드
-    response.json({ invite_code: inviteCode });
+    var inviteCode = await generateRoomId(); //초대 코드
+    var like_template_list = await getLikeTemplate(request.body.host_id);  //좋아요 누른 템플릿 리스트
+
+    response.json({
+        invite_code: inviteCode,
+        template_list: like_template_list
+    });
 });
 
-function generateRoomId() {  //방 아이디(초대 코드) 생성
+async function generateRoomId() {  //방 아이디(초대 코드) 생성
     const length = 6;  //초대 코드 길이
     min = Math.pow(10, length - 1);
     max = Math.pow(10, length) - 1;
+    let code;
 
-    return new Promise(async (resolve, reject) => {
-        let code;
-
-        do {
-            code = Math.floor(Math.random() * (max - min + 1)) + min;  //랜덤 숫자 생성
-        } while (await check_room_id(code));
-
-        resolve(code);
-    });
+    do {
+        code = Math.floor(Math.random() * (max - min + 1)) + min; // 랜덤 숫자 생성
+    } while (await check_room_id(code));
+    
+    return code;
 }
 
-function check_room_id(code) {  //방 아이디 중복 체크
+async function check_room_id(code) {  //방 아이디 중복 체크
     return new Promise((resolve, reject) => {
         db.query(`SELECT * FROM rollingpaper_room WHERE room_id = ${code}`, function (error, results, fields) {
             if (error) {
@@ -36,6 +39,19 @@ function check_room_id(code) {  //방 아이디 중복 체크
                 resolve(false);
             } else {  //중복 O
                 resolve(true);
+            }
+        });
+    });
+}
+
+function getLikeTemplate(id){  //좋아요 누른 템플릿 가져오기
+    sql = `SELECT like_template_list FROM user WHERE id = '${id}'`; 
+    return new Promise((resolve, reject) => {
+        db.query(sql, function (error, results, fields) {
+            if (error) {
+              reject({ error: '템플릿 불러오기 실패' });
+            } else {
+              resolve(results);
             }
         });
     });
@@ -52,25 +68,23 @@ app.post('/create-room', (request, response)=>{
     insertRoomInfo(sql);
 
     response.status(200).send({
+        host_id: request.body.host_id,
+        title: request.body.title,
+        public: request.body.public,
+        release_date: request.body.release_date,
+        template_color: request.body.template_color,
+        template_image: request.body.template_image,
+        room_id: request.body.room_id,
+        participation_request: request.body.participation_request,
+        participant_list: request.body.participant_list
     });
 });
 
 function insertRoomInfo(sql) {  //데이터베이스에 방 정보 저장
-
     db.query(sql, function (error, results, fields) {
-        if (error) console.error(error);
-        console.log(results);
+        if (error) response.status(500).json({ error: '방 저장 오류' });
     });
-
-    db.query("SELECT * FROM rollingpaper_room", function (error, results, fields) {
-        if (error) {
-            console.error(error);
-            response.status(500).send("테이블 조회 오류");
-        } else {
-            console.log(results);
-        }
-    });
-};
+}
 
 app.listen(port, () => {
     console.log(`${port}번 포트에서 서버 실행 중`);
